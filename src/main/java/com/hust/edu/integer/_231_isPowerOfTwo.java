350343a22158753497d99e9bfd50bb98bda9269d
MasterTj <798750509@qq.com>
2018-11-14 19:58:35 +0800

    LetCode231 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。 2018/11/14 19:58

---
+++ src/main/java/com/hust/edu/integer/_231_isPowerOfTwo.java
@@ -0,0 +1,19 @@
+package com.hust.edu.integer;
+
+/**
+ * locate com.hust.edu.integer
+ * Created by MasterTj on 2018/11/14.
+ * LetCode231 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+ */
+public class _231_isPowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        if( n%2 == 0)
+            return isPowerOfTwo(n/2);
+        else
+            return false;
+    }
+}

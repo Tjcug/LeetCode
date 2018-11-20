d38fc09f775e8456d6f818109c6d7cee0ccaab15
MasterTj <798750509@qq.com>
2018-11-20 20:49:20 +0800

    LetCode 206 反转一个单链表。 2018/11/20 20:49

---
+++ src/main/java/com/hust/edu/list/_206_reverseList.java
@@ -0,0 +1,34 @@
+package com.hust.edu.list;
+
+import java.util.Stack;
+
+/**
+ * locate com.hust.edu.list
+ * Created by MasterTj on 2018/11/20.
+ * LetCode 206 反转一个单链表。
+ */
+public class _206_reverseList {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp= head;
+        Stack<Integer> stack=new Stack<>();
+        while (temp!=null){
+            stack.push(temp.val);
+            temp=temp.next;
+        }
+
+        ListNode dumpyNode= new ListNode(0);
+        temp=dumpyNode;
+        while (stack.isEmpty()){
+            Integer pop = stack.pop();
+            temp.next=new ListNode(pop);
+            temp=temp.next;
+        }
+        return dumpyNode.next;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}

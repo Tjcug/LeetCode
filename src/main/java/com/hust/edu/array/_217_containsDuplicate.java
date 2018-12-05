679fb2bb2f5466976c195e511a4f0c64ffec0bd7
MasterTj <798750509@qq.com>
2018-12-05 20:52:27 +0800

    LetCode 217 存在重复元素.... 2018/12/5 20:52

---
+++ src/main/java/com/hust/edu/array/_217_containsDuplicate.java
@@ -0,0 +1,22 @@
+package com.hust.edu.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * locate com.hust.edu.array
+ * Created by MasterTj on 2018/12/5.
+ * LetCode 217 存在重复元素
+ */
+public class _217_containsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            } else
+                set.add(num);
+        }
+        return false;
+    }
+}

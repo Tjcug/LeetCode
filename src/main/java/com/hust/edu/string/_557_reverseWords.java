40abdabaf90ef088f6ac6bcff2c85e930970e07e
MasterTj <798750509@qq.com>
2018-12-05 21:02:39 +0800

    LetCode 557 反转字符串中的单词.... 2018/12/5 21:02

---
+++ src/main/java/com/hust/edu/string/_557_reverseWords.java
@@ -0,0 +1,16 @@
+package com.hust.edu.string;
+
+/**
+ * locate com.hust.edu.string
+ * Created by MasterTj on 2018/12/5.
+ * LetCode 557 反转字符串中的单词
+ */
+public class _557_reverseWords {
+    public String reverseWords(String s) {
+        StringBuilder sb=new StringBuilder();
+        for (String s1 : s.split(" ")) {
+            sb.append(new StringBuffer(s1).reverse()).append(" ");
+        }
+        return sb.deleteCharAt(sb.length()-1).toString();
+    }
+}

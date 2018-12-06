9726e102a493e455dab75a569aab1adf5c32bd38
MasterTj <798750509@qq.com>
2018-12-06 22:14:13 +0800

    LetCode 122 买卖股票的最佳时机2 2018/12/6 22:14

---
+++ src/main/java/com/hust/edu/algorithm/_122_maxProfit.java
@@ -0,0 +1,28 @@
+package com.hust.edu.algorithm;
+
+/**
+ * locate com.hust.edu.algorithm
+ * Created by MasterTj on 2018/12/6.
+ * LetCode 122 买卖股票的最佳时机2
+ */
+public class _122_maxProfit {
+    public int maxProfit(int[] prices) {
+        int maxVaule=0;
+        int pos=0;
+        int first,second;
+
+        while (pos<prices.length) {
+            while (pos+1<prices.length && prices[pos + 1] < prices[pos])
+                pos++;
+            first = pos;
+            pos++;
+            while (pos+1<prices.length && prices[pos + 1] > prices[pos])
+                pos++;
+            second = pos;
+            if(first<prices.length && second<prices.length)
+                maxVaule += prices[second] - prices[first];
+        }
+
+        return maxVaule;
+    }
+}

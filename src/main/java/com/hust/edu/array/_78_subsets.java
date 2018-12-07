3f5632f7e4cbcb07845174a95f88444e581e15c6
MasterTj <798750509@qq.com>
2018-12-07 12:05:20 +0800

    LetCode 78 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）.... 2018/12/7 12:06

---
+++ src/main/java/com/hust/edu/array/_78_subsets.java
@@ -0,0 +1,30 @@
+package com.hust.edu.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * locate com.hust.edu.array
+ * Created by MasterTj on 2018/12/7.
+ * LetCode 78 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ */
+public class _78_subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result=new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int size = result.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> list = result.get(j);
+                List<Integer> temp=new ArrayList<>();
+                temp.addAll(list);
+                temp.add(nums[i]);
+                result.add(temp);
+            }
+            List<Integer> numList=new ArrayList<>();
+            numList.add(nums[i]);
+            result.add(numList);
+        }
+        result.add(new ArrayList<>());
+        return result;
+    }
+}

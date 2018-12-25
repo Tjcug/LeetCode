0982c9fd08f01b1e44d1da5ffe30b66fb435e93d
MasterTj <798750509@qq.com>
2018-12-25 18:02:28 +0800

    LetCode 415 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。。。  2018/12/25 18:02

---
+++ src/test/java/com/hust/edu/string/_415_addStringsTest.java
@@ -0,0 +1,16 @@
+package com.hust.edu.string;
+
+import org.junit.Test;
+
+/**
+ * locate com.hust.edu.string
+ * Created by MasterTj on 2018/12/25.
+ */
+public class _415_addStringsTest {
+    @Test
+    public void addStrings() throws Exception {
+        _415_addStrings addStrings=new _415_addStrings();
+        String s = addStrings.addStrings("999", "1651654");
+        System.out.println(s);
+    }
+}

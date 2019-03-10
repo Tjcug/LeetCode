02c6d12e23de0123ddde13cd271fb61782fb15ea
MasterTj <798750509@qq.com>
2019-03-10 17:32:35 +0800

    Offer 360 。。。  2019/3/7 22:24

---
+++ src/main/java/com/hust/edu/_pdd/Main3.java
@@ -0,0 +1,32 @@
+package com.hust.edu._pdd;
+
+import java.util.Scanner;
+
+/**
+ * locate com.hust.edu._pdd
+ * Created by MasterTj on 2019/3/10.
+ */
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        while (in.hasNext()){
+            int N= in.nextInt();
+            int d= in .nextInt();
+            int a[]=new int[N];
+            int b[]=new int[N];
+            for (int i = 0; i < N; i++) {
+                a[i]=in.nextInt();
+                b[i]=in.nextInt();
+            }
+
+            int max=Integer.MIN_VALUE;
+            for (int i = 0; i < N; i++) {
+                for (int j = i+1; j < N; j++) {
+                    if(Math.abs(a[i]-a[j])>=d){
+                        max=Math.max(max,b[i]+b[j]);
+                    }
+                }
+            }
+        }
+    }
+}

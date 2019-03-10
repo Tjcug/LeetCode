b97d651d2dbf892bd9edfb64b1bfb4f5ce6e403f
MasterTj <798750509@qq.com>
2019-03-10 17:22:45 +0800

    Offer 360 。。。  2019/3/7 22:24

--- src/main/java/com/hust/edu/_pdd/Main2.java
+++ src/main/java/com/hust/edu/_pdd/Main2.java
@@ -1,6 +1,6 @@
 package com.hust.edu._pdd;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -11,11 +11,37 @@ public class Main2 {
     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
         while (in.hasNext()){
+            HashMap<Character,Integer> map=new HashMap<>();
             String str = in.nextLine();
-            String lowerStr = str.toLowerCase();
-            char[] chars = lowerStr.toCharArray();
-            Arrays.sort(chars);
+            String lowerCase = str.toLowerCase();
+            char[] chars = lowerCase.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                if(map.get(chars[i])==null)
+                    map.put(chars[i],1);
+                else {
+                    Integer integer = map.get(chars[i]);
+                    map.put(chars[i],++integer);
+                }
+            }
 
-            System.out.println(chars[0]);
+            int index = 0;
+            for (int i = 1; i < chars.length; i++) {
+                if((int)chars[index] > (int)chars[i]){
+                    int temp=i-1;
+                    while (temp>index){
+                        Integer integer = map.get(chars[temp]);
+                        if(integer>1){
+                            temp--;
+                            map.put(chars[temp],--integer);
+                        }else
+                            break;
+                    }
+                    if(temp==index)
+                        index=i;
+                }
+            }
+
+            System.out.println(chars[index]);
         }
-    }}
+    }
+}

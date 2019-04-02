6d5750e8dcf5e3eb24e82c6e6fbed7026b294378
MasterTj <798750509@qq.com>
2019-04-02 21:42:45 +0800

    baidu  2019/4/2 20:57

--- src/main/java/com/hust/edu/baidu/Soultion1.java
+++ src/main/java/com/hust/edu/baidu/Soultion1.java
@@ -3,33 +3,33 @@ package com.hust.edu.baidu;
 /**
  * locate com.hust.edu.baidu
  * Created by MasterTj on 2019/4/2.
+ * 百度笔试第一题
  */
 public class Soultion1 {
-    private static int result[];
 
     public static int minGasStation(int numsOfGS,int[] distOfGS,int [] allowedGasoline, int distance, int initialGasoline){
         int[] dp=new int[numsOfGS+1];
-        result=new int[numsOfGS+1];
+        int[] result=new int[numsOfGS+1];
         dp[0]=initialGasoline;
         result[0]=0;
         for (int i = 1; i <= numsOfGS; i++) {
-            minGasStationHelper(dp,i,distOfGS,allowedGasoline);
+            minGasStationHelper(dp,result,i,distOfGS,allowedGasoline);
         }
-        if(dp[numsOfGS-1]<distance)
+        if(dp[numsOfGS]+allowedGasoline[numsOfGS-1]<distance)
             return -1;
         return result[numsOfGS];
     }
 
-    private static void minGasStationHelper(int[] dp,int index,int[] distOfGS,int [] allowedGasoline){
+    private static void minGasStationHelper(int[] dp,int[] result,int index,int[] distOfGS,int [] allowedGasoline){
         int low=index;
-        int high=distOfGS.length-1;
-        while (low<=high){
-            if(dp[low-1]>=distOfGS[high]){
-                break;
-            }else
-                high--;
+        for (int i = 0; i < low; i++) {
+            if(dp[i]>=distOfGS[index-1]){
+                dp[index]=dp[i]+allowedGasoline[index-1];
+                result[index]=result[i]+1;
+                return;
+            }
         }
-        dp[index]=dp[low-1]+allowedGasoline[high];
+        dp[index]=-1;
     }
 
     public static void main(String[] args) {

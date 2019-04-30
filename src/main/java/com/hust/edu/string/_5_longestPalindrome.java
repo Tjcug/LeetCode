8347f26e5b927e2c907791d2f3152c17d4bb891a
MasterTj <798750509@qq.com>
2019-04-30 18:32:10 +0800

    leetcode  2019/4/30 17:51

--- src/main/java/com/hust/edu/string/_5_longestPalindrome.java
+++ src/main/java/com/hust/edu/string/_5_longestPalindrome.java
@@ -35,7 +35,7 @@ public class _5_longestPalindrome {
     public String longestPalindrome(String s) {
         if(s.length()==0)
             return "";
-       int start=0, end=0;
+        int start=0, end=0;
         for (int i = 0; i <s.length(); i++) {
             int i1 = expandAroundCenter(s, i, i);
             int i2 = expandAroundCenter(s, i, i + 1);
